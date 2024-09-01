ec902135d0d626b2d46a3d532b8372e275b4c6f2
dcarello <danielcarello@gmail.com>
2024-09-01 00:17:19 -0700

    Board is printing

--- TicTacToe/src/mechanicsBE/slTTTBoard.java
+++ TicTacToe/src/mechanicsBE/slTTTBoard.java
@@ -1,11 +1,34 @@
 package mechanicsBE;
 
 public class slTTTBoard {
+
+    private char [][] board = new char[3][3];
+
+
+    public slTTTBoard(){
+
+        // for the rows
+        for (int i = 0; i < 3; i++){
+            // for the columns
+            for (int j = 0; j < 3; j++){
+                board[i][j] = '-';
+            }
+        }
+    }
+
     public void play(){
         System.out.println("Play");
     }
 
     public void printBoard(){
-        System.out.println("Print Board");
+        // for the rows
+        for (int i = 0; i < 3; i++){
+            // for the columns
+            for (int j = 0; j < 3; j++){
+                System.out.print(board[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
     }
 }
