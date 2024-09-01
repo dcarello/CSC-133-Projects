677ef2945abca949cb764a101e5776d86cb549be
dcarello <danielcarello@gmail.com>
2024-08-31 23:50:45 -0700

    Initial File Setup for TicTacToe

---
+++ TicTacToe/src/mechanicsBE/slTTTBoard.java
@@ -0,0 +1,11 @@
+package mechanicsBE;
+
+public class slTTTBoard {
+    public void play(){
+        System.out.println("Play");
+    }
+
+    public void printBoard(){
+        System.out.println("Print Board");
+    }
+}
